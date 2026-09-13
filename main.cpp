2edd1b8fea7e165c6f7daa98dc16589c0722dc3d
sliman <sliman77ammari@gmail.com>
2026-09-13 15:44:19 +0300

    init main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int main() {
+   
+	cout << "\n\nCalculater Application\n\n";
+
+	int a, b;
+	
+	cout << "Add 2+4 = " << add(2, 4) << endl;
+
+
+
+	return 0;
+}
